b00719c62bb2e2db144b7ec2ea1a878e08ecfc73
Grzegorz Milczuk <w.luk1972@wp.pl>
2018-07-01 10:24:53 +0200

    first commit

---
+++ src/app/hero.service.ts
@@ -0,0 +1,20 @@
+import { MessageService } from './message.service';
+
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+  
+   getHeroes(): Observable<Hero[]> {
+	    this.messageService.add('HeroService: fetched heroes');
+	   return of(HEROES);
+   }
+  
+}
